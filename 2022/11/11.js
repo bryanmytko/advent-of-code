25390d0dddeee1cb8e7179a88ae9dbcb5d69759d
Bryan Mytko <bryanmytko@gmail.com>
2022-12-12 02:07:31 -0800

    Add Day 11

--- 2022/11/11.js
+++ 2022/11/11.js
@@ -3,13 +3,15 @@ const fs = require("fs");
 const file = "./testInput.txt";
 const input = fs.readFileSync(file, "utf-8");
 const lines = input.split("\n");
-
-const rounds = 1;
 const monkeys = [];
+
+let allDivisors = 1;
 
 /* Generate initial monkey conditions */
 lines.forEach((line, index) => {
   if (line.startsWith("Monkey")) {
+    allDivisors *= parseInt(lines[index + 3].match(/(\d+)/)[1]);
+
     monkeys.push({
       items: lines[index + 1]
         .split(":")[1]
@@ -22,17 +24,42 @@ lines.forEach((line, index) => {
         return eval(`${l}${data[2]}${r}`);
       },
       test: (n) => {
-        const div = parseInt(lines[index + 3].match(/(\d+)/)[1]);
         const truthy = parseInt(lines[index + 4].match(/(\d+)/)[1]);
         const falsey = parseInt(lines[index + 5].match(/(\d+)/)[1]);
+        const div = parseInt(lines[index + 3].match(/(\d+)/)[1]);
+
         return n % div === 0 ? truthy : falsey;
       },
     });
   }
 });
 
-for (let i = 0; i < rounds; i++) {
-  monkeys.forEach((monkey) => {
-    console.log(monkey);
-  });
-}
+const monkeyGame = (rounds, ridiculous = false) => {
+  const counted = Array(monkeys.length).fill(0);
+
+  for (let i = 0; i < rounds; i++) {
+    monkeys.forEach((monkey, index) => {
+      monkey.items.forEach((item) => {
+        item = monkey.operation(item);
+
+        if (!ridiculous) item = Math.floor(item / 3);
+
+        item %= allDivisors;
+        monkeys[monkey.test(item)].items.push(item);
+        counted[index]++;
+      });
+      monkey.items.length = 0;
+    });
+  }
+
+  // console.log(counted.sort((a, b) => b - a));
+
+  return counted
+    .sort((a, b) => b - a)
+    .slice(0, 2)
+    .reduce((a, b) => a * b);
+};
+
+// Only run one or the other
+// console.log("Part 1.", monkeyGame(20));
+console.log("Part 2.", monkeyGame(10000, true));
