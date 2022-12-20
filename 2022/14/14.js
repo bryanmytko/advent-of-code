832ca1e76221ef1b0c48af3feb740dc1dc20230a
Bryan Mytko <bryanmytko@gmail.com>
2022-12-19 23:29:32 -0800

    Add test drawing

--- 2022/14/14.js
+++ 2022/14/14.js
@@ -1,14 +1,16 @@
 const fs = require("fs");
-// const file = "./input.txt";
-const file = "./testInput.txt";
+const file = "./input.txt";
+// const file = "./testInput.txt";
 
 const lines = fs
   .readFileSync(file, "utf-8")
   .split(/\n/)
   .filter((line) => line !== "");
 
-const ROWS = 600;
-const COLS = 600;
+const ROWS = 400;
+const COLS = 400;
+// const ROWS = 20;
+// const COLS = 20;
 const grid = [];
 
 for (let i = 0; i < COLS; i++) {
@@ -40,19 +42,65 @@ lines.forEach((line) => {
   });
 });
 
-/* Drop sand */
-while (1) {
-  const drop = [500, 0];
+const timer = (ms) => new Promise((res) => setTimeout(res, ms));
 
-  /* each "second" of sand dropping */
-  while (true) {
-    const [dropx, dropy] = drop;
-    // TODO
-    if (grid[dropy][dropx] === "#") break;
-    if (grid[dropy + 1][dropx] === "#") break;
+const main = async () => {
+  /* Drop sand */
+  while (1) {
+    // const drop = [500, 0];
+    let drop = [6, 0];
+
+    /* each "second" of sand dropping */
+    while (true) {
+      const [dropx, dropy] = drop;
+
+      /* Draw */
+      console.clear();
+      console.log(drop);
+      grid.forEach((row) => {
+        console.log(row.join(""));
+      });
+      await timer(100);
+
+      drop[1]++;
+
+      // hit air
+      if (grid[dropy + 1][dropx] === ".") {
+        console.log("air...");
+      } else if (grid[dropy + 1][dropx - 1] === ".") {
+        console.log("hit wall, fall left");
+        grid[dropy + 1][dropx - 1] = "o";
+        break;
+      } else if (grid[dropy + 1][dropx + 1] === ".") {
+        console.log("hit wall, fall right");
+        grid[dropy + 1][dropx + 1] = "o";
+        break;
+      } else {
+        console.log("hit wall, fall left");
+        grid[dropy][dropx] = "o";
+        break;
+      }
+      //
+      // // hit a rock with something to the right and nothing to the left
+      // else if (
+      //   grid[dropy + 1][dropx] === "o" &&
+      //   grid[dropy + 1][dropx + 1] !== "."
+      // ) {
+      //   grid[dropy + 1][dropx + 1] = "o";
+      //   continue;
+      // }
+      //
+      // // hit a rock with something to the left and right
+      // else if (grid[dropy + 1][dropx] === "o") {
+      //   grid[dropy][dropx] = "o";
+      //   continue;
+      // } else break;
+    }
   }
-}
+};
+
+// main();
 
 grid.forEach((row) => {
-  // console.log(row.join(""));
+  console.log(row.join(""));
 });
