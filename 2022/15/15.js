a214f356efee5e6db871ff0c3391e92f93fa608d
Bryan Mytko <bryanmytko@gmail.com>
2022-12-21 16:45:55 -0800

    Continue working part 1

--- 2022/15/15.js
+++ 2022/15/15.js
@@ -8,13 +8,32 @@ const input = fs
   .filter((n) => n !== "");
 const coords = [];
 
+// https://en.wikipedia.org/wiki/Taxicab_geometry
+const taxiCabDiff = (p1, p2) => Math.abs(p1.x - p2.x) + Math.abs(p1.y - p2.y);
+const seen = new Set();
+
 input.forEach((line) => {
   const nums = line.match(/(\-?\d+)/gi).map(Number);
+  const sensor = { x: nums[0], y: nums[1] };
+  const beacon = { x: nums[2], y: nums[3] };
 
   coords.push({
-    sensor: { x: nums[0], y: nums[1] },
-    beacon: { x: nums[2], y: nums[3] },
+    sensor,
+    beacon,
+    diff: taxiCabDiff(sensor, beacon),
   });
+});
+
+coords.forEach((coord) => {
+  const { x, y } = coord.sensor;
+  // 2,18
+  // grid[18][2]
+  for (i = 0; i < coord.diff; i++) {
+    seen.add([x + i, y]);
+    seen.add([x - i, y]);
+    seen.add([x + i, y + i]);
+    seen.add([x + i, y - i]);
+  }
 });
 
 console.log(coords);
