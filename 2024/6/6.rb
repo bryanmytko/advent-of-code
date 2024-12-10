01d947dfb9cf9ac45647345ef524165dba244b38
bryan mytko <bryanmytko@gmail.com>
2024-12-10 17:36:12 -0500

    Add 2024 Day 6 Part 1

--- 2024/6/6.rb
+++ 2024/6/6.rb
@@ -1,16 +1,13 @@
 # frozen_string_literal: true
 
-file = File.open('./testInput.txt').readlines.map(&:chomp)
-# file = File.open('./input.txt').readlines.map(&:chomp)
-
-p file
+# file = File.open('./testInput.txt').readlines.map(&:chomp)
+file = File.open('./input.txt').readlines.map(&:chomp)
 
 # class Gallivant
 class Gallivant
   DIRECTIONS = %w[up right down left].freeze
 
-  attr_accessor :cur_direction
-  attr_reader :pos
+  attr_reader :map, :pos, :cur_direction
 
   def initialize(map)
     @cur_direction = 0
@@ -19,50 +16,56 @@ class Gallivant
   end
 
   def start
-    while next_pos_valid?
-      move
-      c = @map[@pos[0]][@pos[1]]
-      p c
-      change_dir if c != '.'
+    write_current
 
+    while peek
+      move
+      write_current
+      change_dir unless ['.', '^', 'X', nil].include? peek
     end
+
+    p map.flatten.join.count('X')
   end
 
   private
 
-  def next_pos_valid?
+  def write_current
+    map[pos[0]][pos[1]] = 'X'
+  end
+
+  def peek
     case cur_direction
     when 0
-      pos[0] - 1 > 0
+      map[pos[0] - 1][pos[1]] if pos[0] - 1 >= 0
     when 1
-      map[@pos[1] + 1]
+      map[pos[0]][pos[1] + 1]
     when 2
-      @pos[0] + 1 < map.size
+      map[pos[0] + 1][pos[1]] if map[pos[0] + 1]
     when 3
-      pos[1] - 1 > 0
+      map[pos[0]][pos[1] - 1]
     end
   end
 
   def change_dir
-    @cur_direction = @cur_direction == DIRECTIONS.size - 1 ? 0 : @cur_direction + 1
+    @cur_direction = cur_direction == DIRECTIONS.size - 1 ? 0 : cur_direction + 1
   end
 
   def find_initial_position
-    @map.each_with_index do |row, index|
+    map.each_with_index do |row, index|
       return [index, row.index('^')] if row.index '^'
     end
   end
 
   def move
-    case @cur_direction
+    case cur_direction
     when 0
-      @pos = [@pos[0] - 1, @pos[1]]
+      @pos = [pos[0] - 1, pos[1]]
     when 1
-      @pos = [@pos[0], @pos[1] + 1]
+      @pos = [pos[0], pos[1] + 1]
     when 2
-      @pos = [@pos[0] + 1, @pos[1] + 1]
+      @pos = [pos[0] + 1, pos[1]]
     when 3
-      @pos = [@pos[0], @pos[1] - 1]
+      @pos = [pos[0], pos[1] - 1]
     end
   end
 end
