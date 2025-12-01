bb676d159a7278c8bacecb1a99dfd9deac697dd8
bryan mytko <bryanmytko@gmail.com>
2025-12-01 01:46:44 -0500

    Add 2025 part I and II

---
+++ 2025/1/1.rb
@@ -0,0 +1,25 @@
+combinations = File.open('./input.txt').readlines.map(&:chomp)
+# combinations = %w(L68 L30 R48 L5 R60 L55 L1 L99 R14 L82)
+
+current = 50
+current2 = 50
+zeros_seen = 0
+
+def rotate(combination)
+  c = combination.scan(/[A-Z]+|\d+/)
+  c[0] == "L" ? -c[1].to_i : c[1].to_i
+end
+
+result = combinations.filter do |c|
+  val = rotate(c)
+  val.abs.times do
+    val > 0 ? current2 += 1 : current2 -= 1
+    zeros_seen += 1 if current2 % 100 == 0
+  end
+
+  current += val
+  current % 100 == 0
+end
+
+p "Part I: #{result.size}"
+p "Part II: #{zeros_seen}"
